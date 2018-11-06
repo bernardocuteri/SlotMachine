4220d96de1d2896e1bd0720c31636109b08fe5ac
tamiralechm <tamri.mesfin@gmail.com>
2018-11-06 17:09:32 +0100

    Update LoginController.java

--- SlotMachine/src/it/unical/asde2018/slot_machine/components/controllers/LoginController.java
+++ SlotMachine/src/it/unical/asde2018/slot_machine/components/controllers/LoginController.java
@@ -8,30 +8,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import it.unical.asde2018.slot_machine.components.services.SlotMachineService;
 
 import it.unical.asde2018.slot_machine.components.services.LoginService;
 
 @Controller
 public class LoginController {
 	
-//	@Autowired
-//	private SlotMachineService slotMachineService;
-//	
+	@Autowired
+	private SlotMachineService slotMachineService;
+	
 	@Autowired
 	private LoginService loginService;
 
-//	@GetMapping("/")
-//	public String goToIndex(Model model, HttpSession session) {
-//		
-//
-//		if (session.getAttribute("user") != null) {
-//
-//			model.addAttribute("posts", slotMachineService.getAllPosts());
-//			return "index";
-//		}
-//		return "login";
-//	}
-//	
+	@GetMapping("/")
+	public String goToIndex(Model model, HttpSession session) {
+ 
+		if (session.getAttribute("user") != null) {
+ 
+		}
+		return "login";
+	}
+	
 	@GetMapping("/login")
 	public String loginAttempt(@RequestParam String username, @RequestParam String password, HttpSession session, Model model) {
 		
