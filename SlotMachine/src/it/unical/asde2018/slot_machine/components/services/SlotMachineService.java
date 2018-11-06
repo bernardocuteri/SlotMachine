19d31276b85541d6600821e8da61aa36e66cb2c4
GitElena <mastriaelena@gmail.com>
2018-11-06 13:12:19 +0100

    first commit

--- SlotMachine/src/it/unical/asde2018/slot_machine/components/services/SlotMachineService.java
+++ SlotMachine/src/it/unical/asde2018/slot_machine/components/services/SlotMachineService.java
@@ -12,18 +12,7 @@ import it.unical.asde2018.slot_machine.model.Player;
 
 @Service
 public class SlotMachineService {
-//	HashMap<Player, Game> games;
-	
-//	@PostConstruct
-//	public void init() {
-////		games = new HashMap<>();
-//	}
 
-//	public Game createGame(Player p) {
-//		Game g = new Game();
-//		games.put(p, g);
-//		return g;
-//	}
 	public Game bet(Player p, int bet) {
 		Game g= new Game();
 		g.random();
