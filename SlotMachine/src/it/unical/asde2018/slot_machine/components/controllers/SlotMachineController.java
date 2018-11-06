d0392520b97a3f17ee6914da4f4e04412c495efc
tamiralechm <tamri.mesfin@gmail.com>
2018-11-06 16:50:11 +0100

    Update SlotMachineController.java

--- SlotMachine/src/it/unical/asde2018/slot_machine/components/controllers/SlotMachineController.java
+++ SlotMachine/src/it/unical/asde2018/slot_machine/components/controllers/SlotMachineController.java
@@ -5,6 +5,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class SlotMachineController {
 	
+		@Autowired
+	SlotMachineService slotMachine;
+	
+	@GetMapping("/result")
+	public String bet(Model model,HttpSession httpSession) {
+ 
+		return "slot-machine";
+	}
 	
 
 	
