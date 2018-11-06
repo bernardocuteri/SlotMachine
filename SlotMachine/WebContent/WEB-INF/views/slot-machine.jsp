17c15f3a02cf8e07efb9b210b5df2c5ef1985fb0
carlo <carlo@MSI>
2018-11-06 13:06:24 +0100

    Rocca Carlo

--- SlotMachine/WebContent/WEB-INF/views/slot-machine.jsp
+++ SlotMachine/WebContent/WEB-INF/views/slot-machine.jsp
@@ -1,9 +1,25 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <title>Insert title here</title>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-<h1>Hello world</h1>
+	<ul class="list-inline">
+		<c:forEach items="${allRandomNumbers}" var="it">
+			<h3></h3>
+
+			<li class="list-inline-item">${it.number}</li>
+		</c:forEach>
+	</ul>
+
+
 </body>
 </html>
--- SlotMachine/src/it/unical/asde2018/slot_machine/components/controllers/LoginController.java
+++ SlotMachine/src/it/unical/asde2018/slot_machine/components/controllers/LoginController.java
@@ -1,15 +1,54 @@
 package it.unical.asde2018.slot_machine.components.controllers;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import it.unical.asde2018.slot_machine.model.Number;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.unical.asde2018.slot_machine.components.services.LoginService;
+import it.unical.asde2018.slot_machine.components.services.SlotMachineService;
 
 @Controller
 public class LoginController {
-	
+
+	@Autowired
+	private LoginService loginService;
+	@Autowired
+	private SlotMachineService slot;
+
 	@GetMapping("/")
-	public String index() {
-		
+	public String index(Model model, HttpSession session) {
+
+		if (session.getAttribute("user") != null) {
+			session.invalidate();
+			model.addAttribute("allRandomNumbers", slot.getAllRandomNumber());
+			return "slot-machine";
+		}
 		return "login";
 	}
 
+	@GetMapping("/login")
+	public String loginAttempt(@RequestParam String username, @RequestParam String password, HttpSession session,
+			Model model) {
+
+		if (loginService.login(username, password)) {
+			session.setAttribute("user", username);
+			return "redirect:/";
+		}
+		model.addAttribute("error", "Wrong credentials!");
+		return "login";
+
+	}
+
+	@GetMapping("/guess")
+	public String guessNumber(@RequestParam Integer num, Model model, HttpSession session) {
+		for (Number number : slot.getAllRandomNumber()) {
+			
+		}
+		return null;
+	}
 }
