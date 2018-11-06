b838619b997ee90d40952c646affabf58516961f
Sindhu <Sindhu@160.97.140.193>
2018-11-06 13:26:32 +0100

    Intermediate Test

--- SlotMachine/WebContent/WEB-INF/views/login.jsp
+++ SlotMachine/WebContent/WEB-INF/views/login.jsp
@@ -1,9 +1,33 @@
+
 <!DOCTYPE html>
+
 <html>
 <head>
-<title>Insert title here</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Login Form</title>
+<h2>Game Login Form</h2>
 </head>
 <body>
-<h1>Hello world</h1>
+<form:form name="submitForm" method="POST">
+<div align="center">
+<table>
+<tr>
+<td>User Name</td>
+<td><input type="text" name="userName" /></td>
+</tr>
+<tr>
+<td>Password</td>
+<td><input type="password" name="password" /></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" value="Submit" /></td>
+</tr>
+</table>
+<div style="color: red">${error}</div>
+
+</div>
+
+</form:form method = "GET" action = "/slot-machine/redirect">
 </body>
 </html>
--- SlotMachine/src/it/unical/asde2018/slot_machine/components/controllers/LoginController.java
+++ SlotMachine/src/it/unical/asde2018/slot_machine/components/controllers/LoginController.java
@@ -1,15 +1,33 @@
 package it.unical.asde2018.slot_machine.components.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.controller.LoginBean;
 
 @Controller
 public class LoginController {
-	
-	@GetMapping("/")
-	public String index() {
-		
-		return "login";
-	}
-
+	  @RequestMapping(value = "/login", method = RequestMethod.GET)
+	  public String init(Model model) {
+	    model.addAttribute("msg", "Please Enter Your Login Details");
+	    return "login";
+	  }
+	  @RequestMapping(method = RequestMethod.GET)
+	  public String submit(Model model, @ModelAttribute("loginBean") LoginBean loginBean) {
+	    if (loginBean != null && loginBean.getUserName() != null & loginBean.getPassword() != null) {
+	      if (loginBean.getUserName().equals("Abdul") && loginBean.getPassword().equals("12345")) {
+	        model.addAttribute("msg", loginBean.getUserName());
+	        return "success";
+	      } else {
+	        model.addAttribute("error", "Invalid Details");
+	        return "login";
+	      }
+	    } else {
+	      model.addAttribute("error", "Please enter Details");
+	      return "login";
+	    }
+	  }
 }
